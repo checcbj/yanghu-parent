16627dfd01f8bde0e6b6c00261d347fadad9c6ec
于志峰 <yuzhifeng@136.com>
2021-02-09 11:26:35 +0800

    linshi

---
+++ yanghu-process-manage/src/main/java/com/yanghu/manage/config/MybatisPlusConfig.java
@@ -0,0 +1,35 @@
+package com.yanghu.manage.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author RunningGan
+ */
+@Configuration
+@MapperScan(basePackages = {"com.yanghu.manage.mapper"})
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+//        return new PaginationInterceptor().setDialectType("oracle");
+//        return new PaginationInterceptor().setDialectType("postgresql");
+        return new PaginationInterceptor().setDialectType("mysql");
+    }
+
+    /**
+     * 乐观锁插件
+     *
+     * @return
+     */
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+}
